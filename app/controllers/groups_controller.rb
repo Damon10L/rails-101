68159cc8f6eda8ef0aa89af364eb6b58be414061
gzn <gzndamon@gmail.com>
2017-04-08 11:31:16 +0800

    move form to partial

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,14 +13,19 @@ class GroupsController < ApplicationController
   end
   def create
     @group = Group.new(group_params)
-    @group.save
-
+    if @group.save
       redirect_to groups_path
+    else
+      render :new
+    end
 end
 def update
   @group=Group.find(params[:id])
-  @group.update(group_params)
+  if @group.update(group_params)
   redirect_to groups_path,notice: "update Success"
+else
+  render :edit
+end
 end
 
 def destroy
